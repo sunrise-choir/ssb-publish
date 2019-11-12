67063843ae941697442035f7c944a27fbd9d7ff7
Piet Geursen <pietgeursen@gmail.com>
2019-11-12 10:28:19 +0100

    Adds tests for second message.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
+use ed25519_dalek::{Keypair, PublicKey, SecretKey, Signature};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256, Sha512};
 use snafu::{ensure, OptionExt, ResultExt, Snafu};
-use ssb_legacy_msg::{Message};
+use ssb_legacy_msg::Message;
 use ssb_legacy_msg_data::json::{from_slice, to_string, to_vec, DecodeJsonError, EncodeJsonError};
 use ssb_legacy_msg_data::value::Value;
 use ssb_legacy_msg_data::LegacyF64;
 use ssb_multiformats::multihash::{Multihash, Target};
 use ssb_multiformats::multikey::{Multikey, Multisig};
-use ed25519_dalek::{Keypair, Signature, PublicKey, SecretKey};
-
 
 #[derive(Debug, Snafu)]
 pub enum Error {
@@ -41,14 +40,16 @@ pub fn publish<T>(
 where
     T: Serialize,
 {
-
     let public_key: PublicKey = PublicKey::from_bytes(&public_key_bytes[..])
         .map_err(|_| snafu::NoneError)
         .context(InvalidPublicKey)?;
     let secret_key: SecretKey = SecretKey::from_bytes(&secret_key_bytes[..])
         .map_err(|_| snafu::NoneError)
         .context(InvalidSecretKey)?;
-    let keypair:    Keypair   = Keypair{secret: secret_key, public: public_key};
+    let keypair: Keypair = Keypair {
+        secret: secret_key,
+        public: public_key,
+    };
     let author = Multikey::from_ed25519(*public_key_bytes);
     let empty_signature = Multisig::from_ed25519([0; 64]);
     let previous_message = match previous_msg_bytes {
@@ -96,7 +97,7 @@ where
         msg.remove("signature".to_owned());
     };
 
-    let signable_bytes = to_string(&message_value, false).unwrap(); 
+    let signable_bytes = to_string(&message_value, false).unwrap();
     let signature_bytes = keypair.sign::<Sha512>(signable_bytes.as_bytes()).to_bytes();
     let signature = Multisig::from_ed25519(signature_bytes);
     let signature_value = Value::String(signature.to_legacy_string());
@@ -112,9 +113,9 @@ where
     let hash = Sha256::digest(&hashable_bytes);
     let key = Multihash::from_sha256(hash.into(), Target::Message);
 
-    let ssb_message = SsbMessage{
+    let ssb_message = SsbMessage {
         key,
-        value: message_value
+        value: message_value,
     };
 
     to_vec(&ssb_message, false)
@@ -156,36 +157,69 @@ struct Contact {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Contact, publish, Content};
-    use ssb_multiformats::multikey::{Multikey};
-    use ssb_verify_signatures;
+    use crate::{publish, Contact, Content};
+    use ssb_multiformats::multikey::Multikey;
     use ssb_validate;
+    use ssb_verify_signatures;
 
-    use rand::Rng;
-    use rand::OsRng;
-    use sha2::Sha512;
     use ed25519_dalek::Keypair;
     use ed25519_dalek::Signature;
+    use rand::OsRng;
+    use rand::Rng;
+    use sha2::Sha512;
 
     #[test]
     fn it_works() {
         let mut csprng: OsRng = OsRng::new().unwrap();
         let keypair: Keypair = Keypair::generate::<Sha512, _>(&mut csprng);
 
-        let contact = Contact{
-            contact: Multikey::from_legacy(b"@9Zf0se86PotjNqaOt9ue8BNBLkGVLQcLNDw/pRQHY3U=.ed25519").unwrap().0,
+        let contact = Contact {
+            contact: Multikey::from_legacy(
+                b"@9Zf0se86PotjNqaOt9ue8BNBLkGVLQcLNDw/pRQHY3U=.ed25519",
+            )
+            .unwrap()
+            .0,
             following: true,
             blocking: false,
         };
         let content = Content::Plain(contact);
-        let msg = publish(content, None, keypair.public.as_bytes() , keypair.secret.as_bytes() , 0.0).unwrap();
+        let msg1 = publish(
+            content,
+            None,
+            keypair.public.as_bytes(),
+            keypair.secret.as_bytes(),
+            0.0,
+        )
+        .unwrap();
 
-        let is_valid = ssb_validate::validate_hash_chain(&msg, None).is_ok();
-        let is_verified = ssb_verify_signatures::verify(&msg).is_ok();
+        let is_valid1 = ssb_validate::validate_hash_chain(&msg1, None).is_ok();
+        let is_verified1 = ssb_verify_signatures::verify(&msg1).is_ok();
 
-        println!("{}", String::from_utf8(msg).unwrap());
+        let contact = Contact {
+            contact: Multikey::from_legacy(
+                b"@9Zf0se86PotjNqaOt9ue8BNBLkGVLQcLNDw/pRQHY3U=.ed25519",
+            )
+            .unwrap()
+            .0,
+            following: false,
+            blocking: false,
+        };
+        let content = Content::Plain(contact);
+        let msg2 = publish(
+            content,
+            Some(&msg1),
+            keypair.public.as_bytes(),
+            keypair.secret.as_bytes(),
+            0.0,
+        )
+        .unwrap();
 
-        assert!(is_valid);
-        assert!(is_verified);
+        let is_valid2 = ssb_validate::validate_hash_chain(&msg2, Some(&msg1)).is_ok();
+        let is_verified2 = ssb_verify_signatures::verify(&msg2).is_ok();
+
+        assert!(is_valid1);
+        assert!(is_verified1);
+        assert!(is_valid2);
+        assert!(is_verified2);
     }
 }
